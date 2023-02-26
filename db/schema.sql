cc1a18cf235742661ef0ee3b50535f46d1835e89
Grace Ocampina <grace.ocampina@ucsf.edu>
2023-02-26 00:45:41 -0800

    updated schema codes

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,27 @@
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 
-USE employees;
+USE employees_db;
+
+CREATE TABLE department(
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+	id INT AUTO_INCREMENT NOT NULL,
+	title VARCHAR(30) NOT NULL,
+    salary DECIMAL(7,0) NOT NULL,
+    department_id INT NOT NULL,
+	PRIMARY KEY (id)
+  );
+  
+CREATE TABLE employee(
+	id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+  );
